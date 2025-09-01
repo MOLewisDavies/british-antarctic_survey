0feda9c76de9696546cc22d89e666a7f363833b1
MOLewisDavies <138601494+MOLewisDavies@users.noreply.github.com>
2025-09-01 12:10:14 +0100

    Update wind_gust.py
    Replaced grid square enumerate with lat lon loop

--- wind_gust.py
+++ wind_gust.py
@@ -8,7 +8,8 @@ main(): runs other functions
 get_stats(): gets data from era5 cube
 mask_cube(): masks cube based on shapefile
 
-"""
+ """
+
 
 import iris
 import glob
@@ -16,6 +17,7 @@ from pathlib import Path
 import seaborn as sns
 import numpy as np
 import pandas as pd
+
 
 ## wind gust era5 files
 gust_f_path = "/data/scratch/lewis.davies/bas/wind"
@@ -44,8 +46,10 @@ def main():
 
             ## checks if csv file exists
             if csv_file.is_file():
-              
+                
+                
                 continue
+
 
             # ...if it doesn't - produces data
             else:
@@ -58,9 +62,9 @@ def get_stats(index, site, shape_file):
     
     ## empty stats
     stats = {"full_date": [], "month": [], "day": [], 
-             "hour": [], "grid_square": [], "wind_gust": []}
-    
-    
+             "hour": [], "latitude": [], "longitude": [],
+             "wind_gust": []}
+
     for file in gust_files:
         
         gust_cube = iris.load_cube(file)
@@ -76,26 +80,30 @@ def get_stats(index, site, shape_file):
             day_str = full_dt.strftime("%d")
             hour_str = full_dt.strftime("%H")
             
-            ## loop through flattened data array 
-            for grid, gust in enumerate(site_cube.data.flatten()):
+            for site_cube in site_cube.slices_over(['longitude', 'latitude']):
                 
-                ## if value is masked, do not add to dict
-                if type(gust) == np.ma.core.MaskedConstant:
-                    
-                    pass
+                lats = site_cube.coord('latitude').points
+                lons = site_cube.coord('longitude').points
                 
-                else:
+                ## loop through flattened data array 
+                for lat, lon, gust in zip(lats, lons, site_cube.data.flatten()):
+
+                    ## if value is masked, do not add to dict
+                    if type(gust) == np.ma.core.MaskedConstant:
+                        
+                        pass
                     
-                    ## add values to dictionary
-                    print(full_dt)
-                    stats["full_date"].append(full_dt)
-                    stats["month"].append(month_str)
-                    stats["day"].append(day_str)
-                    stats["hour"].append(hour_str)
-                    stats["grid_square"].append(grid)
-                    stats["wind_gust"].append(gust)
-    
-    
+                    else:
+                         
+                        print(full_dt)
+                        stats["full_date"].append(full_dt)
+                        stats["month"].append(month_str)
+                        stats["day"].append(day_str)
+                        stats["hour"].append(hour_str)
+                        stats["latitude"].apppend(lat)
+                        stats["longitude"].append(lon)
+                        stats["wind_gust"].append(gust)
+
     ## turn dict into dataframe   
     site_df = pd.DataFrame(stats)
     
