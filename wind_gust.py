41d631e0134075ea28f389221bc67aa1244b0f26
MOLewisDavies <138601494+MOLewisDavies@users.noreply.github.com>
2025-11-19 12:07:23 +0000

    Update wind_gust.py
    corrected f'{month}' to Total in make_area_heatmap

--- wind_gust.py
+++ wind_gust.py
@@ -203,11 +203,11 @@ def make_area_heatmap(area_df):
     ax.minorticks_on() """
 
     ## define precip data
-    gust_data = area_df[f"{month}"]
+    gust_data = area_df["total"]
 
     ## create normalised data column
-    area_df["norm"] = (area_df[f"{month}"] - area_df[f"{month}"].min()) / (
-        area_df[f"{month}"].max() - area_df[f"{month}"].min()
+    area_df["norm"] = (area_df["total"] - area_df["total"].min()) / (
+        area_df["total"].max() - area_df["total"].min()
     )
     gust_norm = area_df["norm"]
 
@@ -234,7 +234,7 @@ def make_area_heatmap(area_df):
     ax.add_collection(patch_collection)
 
     ## add title
-    ax.set_title(f"{month.capitalize()} Flyable Days by Grid Point: All Sites")
+    ax.set_title(f"Total Flyable Days by Grid Point: All Sites")
 
     ## add colourbar
     plt.colorbar(
